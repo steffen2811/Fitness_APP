d88199743c20859e903bc6a3adf90b6da55ed391
Tobias Brammer Fredriksen <tobf@mac161442.tv2.local>
2018-10-25 10:22:51 +0200

    added ios app

---
+++ src/APP_IOS/Fitness app/Fitness app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness app
+//
+//  Created by Tobias Brammer Fredriksen on 25/10/2018.
+//  Copyright © 2018 Tobias Brammer Fredriksen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
