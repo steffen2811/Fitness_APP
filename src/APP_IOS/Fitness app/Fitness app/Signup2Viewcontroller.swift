177727cf4fd47416ff306883278618cc20f95e21
Tobias Brammer Frederiksen <tobf@tv2.dk>
2019-01-08 12:34:19 +0100

    added all info to signup and find lokation

--- src/APP_IOS/Fitness app/Fitness app/Signup2Viewcontroller.swift
+++ src/APP_IOS/Fitness app/Fitness app/Signup2Viewcontroller.swift
@@ -8,18 +8,47 @@
 
 import Foundation
 import UIKit
+import MapKit
 
-class Signup2Viewcontroller: UIViewController {
+class Signup2Viewcontroller: UIViewController, CLLocationManagerDelegate {
     
     var base64SignupString = ""
     
+    var locationManager: CLLocationManager!
+    
     @IBOutlet weak var NameText: UITextField!
     @IBOutlet weak var TimeSpendText: UITextField!
+    @IBOutlet weak var AgeText: UITextField!
+    @IBOutlet weak var MobileText: UITextField!
+    @IBOutlet weak var Primary_Sports: UITextField!
+    @IBOutlet weak var Sport_Level: UITextField!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(base64SignupString)
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    var center = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var location = locations.last as! CLLocation
+        
+        center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        print(center.latitude)
+        print(center.longitude)
     }
     
     
@@ -29,7 +58,7 @@ class Signup2Viewcontroller: UIViewController {
     
     func Signup() {
         
-        let parameters = ["timeSpendPerWeek": TimeSpendText.text, "Name": NameText.text] as [String : Any]
+        let parameters = ["timeSpendPerWeek": TimeSpendText.text, "used_facebook_login": 0, "Full_name": NameText.text, "Location_long": center.longitude, "Location_Lat": center.latitude, "Age": AgeText.text, "mobile": MobileText.text, "Primary_Sports": Primary_Sports.text, "Sport_Level": Sport_Level.text ] as [String : Any]
         
         //Create the url
         let url = URL(string: "http://localhost:3333/users/create")
