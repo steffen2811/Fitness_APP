3d777c75b66ba6c26a0c52612847601ee182b37a
Tobias Brammer Frederiksen <tobf@tv2.dk>
2019-01-17 10:01:00 +0100

    added ProfilePicstruck

---
+++ src/APP_IOS/Fitness app/Suportfiles/ProfilePicstruct.swift
@@ -0,0 +1,9 @@
+//
+//  ProfilePicstruct.swift
+//  Fitness app
+//
+//  Created by Tobias Brammer Fredriksen on 16/01/2019.
+//  Copyright © 2019 Tobias Brammer Fredriksen. All rights reserved.
+//
+
+import Foundation
