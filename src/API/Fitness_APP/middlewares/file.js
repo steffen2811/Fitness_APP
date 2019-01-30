a338d5c2f992e1505bf9d8dc394271614d5d3f58
Steffen Thomsen <54409@grundfos.com>
2019-01-30 12:29:23 +0100

    Fix profile picture NOJIRA-

--- src/API/Fitness_APP/middlewares/file.js
+++ src/API/Fitness_APP/middlewares/file.js
@@ -1,4 +1,5 @@
 var base64Img = require('base64-img');
+var path = require('path');
 
 // Upload picture from base64 string
 function fileUpload(mode) {
@@ -9,14 +10,15 @@ function fileUpload(mode) {
                 req.body.profilePicture = "data:image/png;base64," + req.body.profilePicture
             }
             //Copy image to picture folder
-            base64Img.img(req.body.profilePicture, process.env.BASE_PATH + '/profilePictures', req.body.email, function (err) {
+            base64Img.img(req.body.profilePicture, process.env.BASE_PATH + '/profilePictures', req.body.email, function (err, filePath) {
                 if (err) {
                     return res.status(500).json({
                         error: err
                     });
                 }
+                var fileName = path.basename(filePath)
                 //Set url to picture in user profile
-                req.body.profileImgPath = process.env.PROFILE_PICTURE_PATH + req.body.email;
+                req.body.profileImgPath = process.env.PROFILE_PICTURE_PATH + fileName;
                 next();
             });
         } else {
